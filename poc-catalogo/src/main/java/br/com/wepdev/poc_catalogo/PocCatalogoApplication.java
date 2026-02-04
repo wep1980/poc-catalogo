85811878289576728f0305861d83acaedcc1b7d4
wepereira <wepereira@tjba.jus.br>
2026-02-04 12:40:14 -0300

    inicio de projeto backend

---
+++ poc-catalogo/src/main/java/br/com/wepdev/poc_catalogo/PocCatalogoApplication.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.poc_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCatalogoApplication.class, args);
+	}
+
+}
---
+++ poc-catalogo/src/test/java/br/com/wepdev/poc_catalogo/PocCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.poc_catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
